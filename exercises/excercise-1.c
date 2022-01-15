4ec47784a3b1b976295e7a0970f9fe2c9a657d79
G Mohammed Thabrez <79008860+mdthabrez@users.noreply.github.com>
2022-01-15 23:34:08 +0530

    Update excercise-1.c

--- exercises/excercise-1.c
+++ exercises/excercise-1.c
@@ -15,10 +15,54 @@ printf("The character in lowercase is : %c\n",c+32);
 printf("Enter a character : ");
 scanf(" %c",&c);
 printf("The ASCII value of the character is : %d\n",c); 
- printf("Enter a string: ");
- scanf(" %s",&s);
- printf("\'%-15s\'\n",s);
- printf("\'%+15s\'\n",s);
- printf("\'% 15s\'\n",s);
- printf("\'%.5s\'\n",s);
+printf("Enter a string: ");
+scanf(" %s",&s);
+printf("\'%-15s\'\n",s);
+printf("\'%+15s\'\n",s);
+printf("\'% 15s\'\n",s);
+printf("\'%.5s\'\n",s);
  }
+
+
+/*
+2.Write a C program that prints the floating point value in exponential format with the 
+following specifications:
+a) Correct to two decimal places
+b) Correct to four decimal places
+c) Correct to eight decimal places
+d) Convert it into double with four decimal places
+*/
+
+#include<stdio.h>
+int main()
+{
+ float v;
+ 
+ printf("Enter a float number : ");
+ scanf("%f",&v);
+ printf("%.2e\n",v);
+ printf("%.4e\n",v);
+ printf("%.8e\n",v);
+ v = (double)v;
+ printf("%.4e\n",v);
+ }
+
+
+/*
+3.Write a C program to read the floating point number. Display the rightmost digit of the 
+integral part of the number.
+*/
+
+#include<stdio.h>
+int main()
+{
+ int i;
+ float f;
+ printf("Enter the float value : ");
+ scanf("%f",&f);
+ i=f;
+ i=i%10;
+ printf("The rightmost digit of the integral part of the number is %d",i);
+}
+
+
