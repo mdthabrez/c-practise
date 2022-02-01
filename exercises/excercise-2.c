24ff4fed0dbda927ae98716bcc9d4d0522386c87
G Mohammed Thabrez <79008860+mdthabrez@users.noreply.github.com>
2022-02-01 23:49:04 +0530

    Update excercise-2.c

--- exercises/excercise-2.c
+++ exercises/excercise-2.c
@@ -118,3 +118,31 @@ int main()
  printf("%.2f", sums);
 }
 
+
+/*
+6. Write a C program to find the largest of four numbers using && operator.
+*/
+#include<stdio.h>
+int main()
+{
+ int a,b,c,d,max;
+ printf("Enter the first number : ");
+ scanf("%d",&a);
+ printf("Enter the second number : ");
+ scanf("%d",&b);
+ printf("Enter the third number : ");
+ scanf("%d",&c);
+ printf("Enter the fourth number : ");
+ scanf("%d",&d);
+ 
+ if(a>=b && a>=c && a>=d)
+ max=a;
+ else if(b>=a && b>=c && b>=d)
+ max=b;
+ else if(c>=a && c>=b && c>=d)
+ max=c;
+ else if(d>=b && d>=c && d>=a)
+ max=d;
+ printf("The largest of the four numbers is %d",max);
+}
+
