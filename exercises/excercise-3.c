0bdeecc5d8347ff124fd7ed73b209a40e5d159ac
G Mohammed Thabrez <79008860+mdthabrez@users.noreply.github.com>
2022-02-22 23:40:52 +0530

    Update excercise-3.c

--- exercises/excercise-3.c
+++ exercises/excercise-3.c
@@ -60,3 +60,35 @@ int main()
  printf("%d ", merge[i]);
  return 0;
 }
+
+
+/*
+3.Write a C program which deletes all the duplicate elements present in the 
+array.
+*/
+#include<stdio.h>
+int main() {
+ int arr[20], i, j, k, size;
+ printf("\nEnter array size : ");
+ scanf("%d", &size);
+ printf("Enter the array : ");
+ for (i = 0; i < size; i++)
+ scanf("%d", &arr[i]);
+ 
+ for (i = 0; i < size; i++) {
+ for (j = i + 1; j < size;) {
+ if (arr[j] == arr[i]) {
+ for (k = j; k < size; k++) {
+ arr[k] = arr[k + 1];
+ }
+ size--;
+ } else
+ j++;
+ }
+ }
+ printf("Array without duplicates : ");
+ for (i = 0; i < size; i++) {
+ printf("%d ", arr[i]);
+ }
+ return (0);
+}
