ff884f82a6addebaed458c459e408cddfa371f83
G Mohammed Thabrez <79008860+mdthabrez@users.noreply.github.com>
2022-03-16 12:41:08 +0530

    Update excercise-3.c

--- exercises/excercise-3.c
+++ exercises/excercise-3.c
@@ -259,3 +259,30 @@ int main() {
  return 0;
 }
 
+
+/*
+8.Write a C program to implement linear search.
+*/
+#include <stdio.h>
+int main()
+{
+ int array[100], search, c, n;
+ printf("Enter number of elements in array : ");
+ scanf("%d", &n);
+ printf("Enter %d integer(s) : ", n);
+ for (c = 0; c < n; c++)
+ scanf("%d", &array[c]);
+ printf("Enter a number to search : ");
+ scanf("%d", &search);
+ for (c = 0; c < n; c++)
+ {
+ if (array[c] == search) 
+ {
+ printf("%d is present at location %d.\n", search, c+1);
+ break;
+ }
+ }
+ if (c == n)
+ printf("%d isn't present in the array.\n", search);
+ return 0;
+}
